87d29cc9cf7f4854c6d1fa548e209b8565071cb3
lopez <lopez@LAPTOP-7NRQOREU>
2021-03-03 13:03:44 -0600

    Calcula el área de un rectangulo

---
+++ src/tipodedatosarea59401/TipoDeDatosArea59401.java
@@ -0,0 +1,17 @@
+package tipodedatosarea59401;
+
+public class TipoDeDatosArea59401 {
+
+    public static void main(String[] args) {
+        
+        int base = 25;
+        
+        int altura = 30;
+        
+        int area = base * altura;
+        
+        System.out.println("El areá del rectangulo es de: " + area);
+        
+    }
+    
+}
